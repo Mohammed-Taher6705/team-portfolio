73d20b33109da57e72df43cc8c3bc609cd6c7e7b
Mohammed Magdy <MohammedTaher.6705@gmail.com>
2026-07-13 17:09:39 +0300

    Move team images to assets

--- src/data/team.js
+++ src/data/team.js
@@ -1,10 +1,14 @@
+import mohamed from "@/assets/team/Mohammed.jpg";
+import ahmed from "@/assets/team/Ahmed.jpg";
+import mahmoud from "@/assets/team/Mahmoud.jpg";
+
 export const team = [
   {
     name: "Mohamed Taher",
     role: "COO | Software Engineer | Embedded & IoT Engineer",
     description:
       "Experienced in software engineering, embedded systems, and IoT development, with a passion for building innovative and scalable technology solutions.",
-    image: "/team/Mohammed.jpg",
+    image: "mohamed",
     github: "https://github.com/Mohammed-Taher6705",
     linkedin: "https://www.linkedin.com/in/mohammed-taher-eng",
   },
@@ -14,7 +18,7 @@ export const team = [
     role: "CEO | Software Engineer",
     description:
       "Leading NovaTech with a vision for innovation while delivering high-quality software solutions and driving business growth.",
-    image: "/team/Ahmed.jpg",
+    image: "ahmed",
     github: "https://github.com/Elshahedasu",
     linkedin: "https://www.linkedin.com/in/ahmed-elshahed-0226172a9/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BTfq53WZQThq7U5kKJNFO%2Fg%3D%3D",
   },
@@ -24,7 +28,7 @@ export const team = [
     role: "CTO | Software Engineer | Cloud Engineer",
     description:
       "Specialized in software architecture and cloud technologies, focused on designing secure, scalable, and high-performance systems.",
-    image: "/team/Mahmoud.jpg",
+    image: "mahmoud",
     github: "https://github.com/mahsn12",
     linkedin: "https://www.linkedin.com/in/mahmoud-hassan-a85647349/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8Tw9sus0Q0CiY1zZLB0nZw%3D%3D",
   },
