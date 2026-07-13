acb16ed9934da246924723cdc5354c363b9043f6
Mohammed Magdy <MohammedTaher.6705@gmail.com>
2026-07-13 23:12:00 +0300

    Fix imported team image references

--- src/data/team.js
+++ src/data/team.js
@@ -8,7 +8,7 @@ export const team = [
     role: "COO | Software Engineer | Embedded & IoT Engineer",
     description:
       "Experienced in software engineering, embedded systems, and IoT development, with a passion for building innovative and scalable technology solutions.",
-    image: "mohamed",
+    image: mohamed,
     github: "https://github.com/Mohammed-Taher6705",
     linkedin: "https://www.linkedin.com/in/mohammed-taher-eng",
   },
@@ -18,7 +18,7 @@ export const team = [
     role: "CEO | Software Engineer",
     description:
       "Leading NovaTech with a vision for innovation while delivering high-quality software solutions and driving business growth.",
-    image: "ahmed",
+    image: ahmed,
     github: "https://github.com/Elshahedasu",
     linkedin: "https://www.linkedin.com/in/ahmed-elshahed-0226172a9/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BTfq53WZQThq7U5kKJNFO%2Fg%3D%3D",
   },
@@ -28,7 +28,7 @@ export const team = [
     role: "CTO | Software Engineer | Cloud Engineer",
     description:
       "Specialized in software architecture and cloud technologies, focused on designing secure, scalable, and high-performance systems.",
-    image: "mahmoud",
+    image: mahmoud,
     github: "https://github.com/mahsn12",
     linkedin: "https://www.linkedin.com/in/mahmoud-hassan-a85647349/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8Tw9sus0Q0CiY1zZLB0nZw%3D%3D",
   },
